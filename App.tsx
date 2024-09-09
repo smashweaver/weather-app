cefa163cecf06a1455f81160625ab389484ab0d4
Jason Jimenez <jasonjimenez@live.com>
2024-09-10 07:57:40 +0800

    Add skia package

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Canvas, Rect, LinearGradient, vec } from '@shopify/react-native-skia';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Rect x={0} y={0} width={200} height={200}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(200, 200)}
+          colors={['red', 'green']}
+        />
+      </Rect>
+    </Canvas>
   );
 }
 
